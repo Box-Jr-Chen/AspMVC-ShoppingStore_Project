dc0ad8fb0ed88b1300c6454ddb2dec19791c4118
BoxChen <box_chen@arplanet.com.tw>
2019-02-13 17:01:50 +0800

    Upadte_20190213

---
+++ ShoppingStore/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,65 @@
+﻿using ShoppingStore.Areas.Admin.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingStore.Areas.Admin.Models.Data;
+namespace ShoppingStore.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
+        {
+            //Declare a list of models
+            List<CategoryVM> categoryVMList;
+
+            using (Dbase db =new Dbase())
+            {
+                //Init the list
+                categoryVMList = db.Categories
+                                .ToArray()
+                                .OrderBy(x => x.Sorting)
+                                .Select(x => new CategoryVM(x))
+                                .ToList();
+            }
+            //Return view with list
+            return View(categoryVMList);
+        }
+
+        //POST: Admin/shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            //Declare id
+            string id;
+
+
+            using (Dbase db = new Dbase())
+            {
+                //Check that the category name is unique
+                if (db.Categories.Any(x => x.Name == catName))
+                    return "titletaken";
+
+                //Init DTO
+                CategoryDTO dto = new CategoryDTO();
+
+                //Add to DTO
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                //Save DTO
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                //Get the id
+                id = dto.Id.ToString();
+             }
+
+            //Return id
+            return id;
+        }
+    }
+}
