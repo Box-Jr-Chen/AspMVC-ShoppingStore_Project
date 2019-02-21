fd090bc6329795066c6901c2c765653be2e76273
box621863 <box621863@gmail.com>
2019-02-21 22:58:23 +0800

    20190221

--- ShoppingStore/App_Start/RouteConfig.cs
+++ ShoppingStore/App_Start/RouteConfig.cs
@@ -13,11 +13,18 @@ namespace ShoppingStore
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "shop", action = "Index",name =  UrlParameter.Optional }, new[] { "ShoppingStore.Controllers" });
+
+            routes.MapRoute("SidebarPartial", "page/SlidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "ShoppingStore.Controllers" });
+            routes.MapRoute("PagesMenuPartial", "page/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] {"ShoppingStore.Controllers"});
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "ShoppingStore.Controllers" });
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "ShoppingStore.Controllers" });
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
---
+++ ShoppingStore/Controllers/PagesController.cs
@@ -0,0 +1,78 @@
+﻿using ShoppingStore.Areas.Admin.Models.Data;
+using ShoppingStore.Areas.Admin.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingStore.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{page}
+        public ActionResult Index(string page="")
+        {
+            //Get/set page slug
+            if (page == "")
+            {
+                page = "home";
+            }
+
+            //Declare model and DTO
+            PageVM  model;
+            PageDTO dto;
+
+
+            //Check if page exists
+            using (Dbase db = new Dbase())
+            {
+                if (!db.Pages.Any(x => x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index", new { page = "" });
+                }
+            }
+
+            //Get page DTO
+            using (Dbase db = new Dbase())
+            {
+                dto = db.Pages.Where(x =>x.Slug ==page).FirstOrDefault();
+            }
+
+
+            //Set page title
+            ViewBag.PageTitle = dto.Title;
+
+            //Check for sidebar
+            if (dto.HasSlidebar == true)
+            {
+                ViewBag.Sliderbar = "Yes";
+            }
+            else
+            {
+                ViewBag.Sliderbar = "No";
+            }
+
+            //Init model
+            model = new PageVM(dto);
+
+            //Return view with model
+            return View(model);
+        }
+
+        public ActionResult PagesMenuPartial()
+        {
+            //Declare a list of PageVM
+            List<PageVM> pageVMList;
+
+            //Get all pages except home
+            using (Dbase db = new Dbase())
+            {
+                pageVMList = db.Pages.ToArray().OrderBy(x => x.Sorting).Where(x => x.Slug != "home").Select(x => new PageVM(x)).ToList();
+            }
+
+                //Return partial view with List
+                return PartialView(pageVMList);
+        }
+    }
+}
