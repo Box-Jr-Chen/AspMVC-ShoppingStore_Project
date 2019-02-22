4ddd8bab5b88004b8b12495de364ca35c59703c4
box621863 <box621863@gmail.com>
2019-02-22 22:37:54 +0800

    20190222

--- ShoppingStore/App_Start/RouteConfig.cs
+++ ShoppingStore/App_Start/RouteConfig.cs
@@ -13,7 +13,9 @@ namespace ShoppingStore
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "shop", action = "Index",name =  UrlParameter.Optional }, new[] { "ShoppingStore.Controllers" });
+            routes.MapRoute("Cart", "Cart/{action}/{id}", new { controller = "Cart", action = "Index",id =  UrlParameter.Optional }, new[] { "ShoppingStore.Controllers" });
+
+            routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "shop", action = "Index", name = UrlParameter.Optional }, new[] { "ShoppingStore.Controllers" });
 
             routes.MapRoute("SidebarPartial", "page/SlidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "ShoppingStore.Controllers" });
             routes.MapRoute("PagesMenuPartial", "page/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] {"ShoppingStore.Controllers"});
---
+++ ShoppingStore/Controllers/CartController.cs
@@ -0,0 +1,74 @@
+﻿using ShoppingStore.Areas.Admin.Models.ViewModels.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingStore.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            //Init the cart list
+            var cart = Session["cart"] as List<CartVM> ?? new List<CartVM>();
+            //Check if cart is empty
+            if (cart.Count ==0 || Session["cart"] == null)
+            {
+                ViewBag.Message = "Yout cart is empty.";
+                return View();
+            }
+
+            //Calculate total and save to ViewBag
+
+            decimal total = 0m;
+
+            foreach (var item in cart)
+            {
+                total += item.Total;
+            }
+
+            ViewBag.GrandTotal = total;
+
+            //Return view with list
+            return View(cart);
+        }
+
+        public ActionResult CartPartial()
+        {
+            //Init CartVM
+            CartVM model = new CartVM();
+
+            //Init quantity
+            int qty = 0;
+
+            //Init price
+            decimal price = 0m;
+
+            //Check for cart session
+            if (Session["cart"] != null)
+            {
+                //Get total qty and price
+                var list = (List<CartVM>)Session["cart"];
+
+                foreach (var item in list)
+                {
+                    qty += item.Quantity;
+                    price += item.Quantity * item.Price;
+                }
+
+            }
+            else
+            {
+                //Or set qty and price to 0
+                model.Quantity = 0;
+                model.Price = 0m;
+            }
+
+            //Return partial view with model
+            return PartialView(model);
+        }
+    }
+}
