0abbea1e8f5bf1514585831610d0e9e28563cf92
box621863 <box621863@gmail.com>
2019-02-26 23:54:23 +0800

    20190226

--- ShoppingStore/Controllers/AccountController.cs
+++ ShoppingStore/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ShoppingStore.Controllers
 {
@@ -17,6 +18,7 @@ namespace ShoppingStore.Controllers
         }
 
         //GET: /account/Login
+        [HttpGet]
         public ActionResult Login()
         {
             //Confirm user is not logged in
@@ -27,6 +29,40 @@ namespace ShoppingStore.Controllers
 
             //Return view
             return View();
+        }
+
+        //POST: /account/Login
+        [HttpPost]
+        public ActionResult Login(LoginUserVM model)
+        {
+            //Check model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //Check if the user is valid
+
+            bool isValid = false;
+
+            using (Dbase db = new Dbase())
+            {
+                if (db.Users.Any(x => x.Username.Equals(model.Username) && x.Password.Equals(model.Password)))
+                {
+                    isValid = true;
+                }
+            }
+
+            if (!isValid)
+            {
+                ModelState.AddModelError("", "Invalid username or password.");
+                return View(model);
+            }
+            else
+            {
+                FormsAuthentication.SetAuthCookie(model.Username, model.RememberMe);
+                return Redirect(FormsAuthentication.GetRedirectUrl(model.Username,model.RememberMe));
+            }
         }
 
         //GET: /account/create-account
@@ -101,6 +137,119 @@ namespace ShoppingStore.Controllers
 
             //Redirect
             return Redirect("~/account/login");
+        }
+
+        //GET: /account/Logout
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("~/account/login");
+        }
+
+        public ActionResult UserNavPartial()
+        {
+            //Get username
+            string username = User.Identity.Name;
+            //Declare model
+            UserNavPartialVM model;
+
+            using (Dbase db =new Dbase())
+            {
+                //Get the user
+                UserDTO dto = db.Users.FirstOrDefault(x => x.Username == username);
+
+                //Build the model
+                model = new UserNavPartialVM()
+                {
+                    FirstName = dto.FirstName,
+                    LastName = dto.LastName
+                };
+
+            }
+
+            //Return partial view with model
+            return PartialView(model);
+        }
+
+        //GET: /account/userProfile
+        [HttpGet]
+        [ActionName("user-profile")]
+        public ActionResult UserProfile()
+        {
+            //Get username
+            string username = User.Identity.Name;
+
+            //Declare model
+            UserProfileVM model;
+
+            using (Dbase db = new Dbase())
+            {
+                //Get user
+                UserDTO dto = db.Users.FirstOrDefault(x =>x.Username ==username);
+
+                //Build model
+                model = new UserProfileVM(dto);
+            }
+
+            //Return view with model
+            return View("UserProfile",model);
+        }
+
+        //GET: /account/userProfile
+        [HttpPost]
+        [ActionName("user-profile")]
+        public ActionResult UserProfile(UserProfileVM model)
+        {
+            //Check model state
+            if(!ModelState.IsValid)
+            {
+                return View("UserProfile",model);
+            }
+
+            //Check if passwords match if need be
+            if (!string.IsNullOrWhiteSpace(model.Password))
+            {
+                if (!model.Password.Equals(model.ConfirmPassword))
+                {
+                    ModelState.AddModelError("","Passwords do not match.");
+                    return View("UserProfile",model);
+                }
+            }
+
+            using (Dbase db = new Dbase())
+            {
+                //Get username
+                string username = User.Identity.Name;
+
+                //Make sure username is unique
+                if (db.Users.Where(x => x.Id != model.Id).Any(x => x.Username == username))
+                {
+                    ModelState.AddModelError("","Username"+ model.Username+" already exists.");
+                    model.Username = "";
+                    return View("UserProfile",model);
+                }
+
+                //Edit DTO
+                UserDTO dto = db.Users.Find(model.Id);
+
+                dto.FirstName = model.FirstName;
+                dto.LastName = model.LastName;
+                dto.EmailAddress = model.EmailAddress;
+                dto.Username = model.Username;
+
+                if (!string.IsNullOrWhiteSpace(model.Password))
+                {
+                    dto.Password = model.Password;
+                }
+                //Save
+                db.SaveChanges();
+            }
+
+            //Set TempData message
+            TempData["SM"] = "You have edited your profile!";
+
+            //Redirect
+            return Redirect("~/account/user-profile");
         }
     }
 }
